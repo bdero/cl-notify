b097676781b4a529d4c308b90c479e429caf724f
Brandon DeRosier <bdero@mit.edu>
2015-05-12 23:14:34 -0400

    Add setup.py

---
+++ cl_notify/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import os
+import logging
+
+log = logging.getLogger()
+logging.basicConfig()
+log.setLevel(logging.DEBUG)
---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+    setuptools_available = True
+except ImportError:
+    from distutils.core import setup
+    setuptools_available = False
+
+
+params = {}
+
+
+setup(
+    name='cl_notify',
+    version='0.0.1',
+    packages=['cl_notify'],
+    author='Brandon DeRosier',
+    author_email='x@bdero.me',
+    description='A desktop notification tool for craigslist searches.',
+    url='https://github.com/bdero/cl-notify',
+    install_requires=[],
+
+    **params
+)
