a39e1ddcd36c2be80cc4619363a8a32050394306
Brandon DeRosier <bdero@mit.edu>
2015-05-21 21:50:09 -0400

    Added classifiers and readme/test_requirements

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 
 try:
-    from setuptools import setup
+    from setuptools import setup, find_packages
     setuptools_available = True
 except ImportError:
-    from distutils.core import setup
+    from distutils.core import setup, find_packages
     setuptools_available = False
 
 
 params = {}
+
+with open('README.md') as readme:
+    params.update({'long_description': readme.read()})
+
+with open('test_requirements.txt') as test_requirements:
+    params.update({'tests_require': test_requirements.read_lines()})
 
 
 setup(
@@ -19,6 +25,13 @@ setup(
     author_email='x@bdero.me',
     description='A desktop notification tool for craigslist searches.',
     url='https://github.com/bdero/cl-notify',
+    classifiers=[
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ],
     install_requires=[
         "python-craigslist",
     ],
